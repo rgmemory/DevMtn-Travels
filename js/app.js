1ae633fdd91795511ee26ffba3c086227e4523ee
Russell Memory <rmemory@Russells-MacBook-Pro.local>
2017-02-25 09:35:28 -0700

    stuff

--- js/app.js
+++ js/app.js
@@ -1,10 +1,49 @@
 angular.module('devmtnTravel', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
+
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
+            })
+
+            .state('adventures', {
+                url: '/adventures',
+                parent: 'home',
+                templateUrl:"../views/about-adventurers.html"
+            })
+
+            .state('contact', {
+                url: '/contact',
+                parent: 'home',
+                templateUrl: "../views/contact.html"
+            })
+
+            .state('packages', {
+                url:'/packages',
+                controller: 'packagesCtrl',
+                templateUrl: "../views/packages.html"
+            })
+
+            .state('booked', {
+                url:'/booked/:xyz',
+                controller: 'bookedCtrl',
+                templateUrl: "../views/booked.html"
+
+            })
+
+            .state('locations', {
+                url:'/locations',
+                templateUrl: "../views/locations.html"
             });
+
+
+
+
+
+
+
+
 
         $urlRouterProvider
             .otherwise('/');
